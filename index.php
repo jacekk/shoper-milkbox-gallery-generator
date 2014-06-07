523e19bfde83e332e5732baef13c3a31f21324f7
JacekK <info@jacek-k.net>
2014-06-07 16:31:42 +0200

    added console support; added file names cleanup

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
+/* settings START */
+
 define('PROCESS_FILES', true);
 define('DIR', './galleries/first/');
 define('THUMB_SIZE', 128);
 define('BIG_MAX_HEI', 1000);
 define('BIG_MAX_WID', 1600);
 define('BASE_URL', 'http://example.com/galleries/');
+define('THUMB_DIR', 'thumbs');
+define('BIG_DIR', 'big');
 
 $thumbTpl = <<<TPL
 <a title="" href="{big}" data-milkbox="milkbox:gall1">
@@ -13,23 +17,27 @@ $thumbTpl = <<<TPL
 </a>
 TPL;
 
-if (is_dir(DIR.'thumbs') === false) {
-    mkdir(DIR.'thumbs');
-}
-if (is_dir(DIR.'big') === false) {
-    mkdir(DIR.'big');
-}
+/* settings END */
 
 require_once './vendor/autoload.php';
 
+$thumbsDir = sprintf('%s%s/', DIR, THUMB_DIR);
+$bigDir = sprintf('%s%s/', DIR, BIG_DIR);
+
 $images = glob(DIR.'*.*');
-if (is_array($images)) {
-    $html = '';
-    $thumbsDir = sprintf('%sthumbs/', DIR);
-    $bigDir = sprintf('%sbig/', DIR);
+
+if (is_array($images) && count($images)) {
+    if (! is_dir($thumbsDir)) {
+        mkdir($thumbsDir);
+    }
+    if (! is_dir($bigDir)) {
+        mkdir($bigDir);
+    }
+    $markup = '';
     foreach ($images as $item) {
         $fileName = basename($item);
         $fileName = strtolower($fileName);
+        $fileName = preg_replace('/[^a-z0-9\-\.]/i', '_', $fileName);
         if (PROCESS_FILES) {
             $thumb = new PHPThumb\GD($item);
             $thumb->resize(BIG_MAX_WID, BIG_MAX_HEI);
@@ -39,12 +47,18 @@ if (is_array($images)) {
         }
         $search = array('{big}','{thumb}');
         $replace = array(
-            sprintf('%sbig/%s', BASE_URL, $fileName),
-            sprintf('%sthumbs/%s', BASE_URL, $fileName),
+            sprintf('%s%s/%s', BASE_URL, THUMB_DIR, $fileName),
+            sprintf('%s%s/%s', BASE_URL, BIG_DIR, $fileName),
         );
-        $html .= str_replace($search, $replace, $thumbTpl);
+        $markup .= str_replace($search, $replace, $thumbTpl);
     }
-    echo htmlspecialchars($html);
+    if (isset($_SERVER['argv'])) {
+        $markupFile = './markup.html';
+        file_put_contents($markupFile, $markup);
+        printf('check file: %s', $markupFile);
+    } else {
+        echo htmlspecialchars($markup);
+    }
 } else {
     printf('no files in: %s', DIR);
 }
